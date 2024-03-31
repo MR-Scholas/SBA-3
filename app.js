639b884d5bd7d7da3e66ea6b0d230929cf200123
MR-Scholas <160638944+MR-Scholas@users.noreply.github.com>
2024-03-31 14:01:04 -0400

    animated background + test image

---
+++ app.js
@@ -0,0 +1,8 @@
+const app=document.getElementById("app")
+let viewer=document.createElement('img')
+viewer.setAttribute("src","vintage-television-with-cut-out-screen-on-isolated-png.webp")
+viewer.setAttribute("alt","television")
+viewer.setAttribute("height","50%")
+app.appendChild(viewer)
+
+viewer.setAttribute("src","https://paint.toys/assets/art-icon.png")
