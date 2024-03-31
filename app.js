33fcf29c0da8c9c1199ad5bc6e5a213824acd93e
MR-Scholas <160638944+MR-Scholas@users.noreply.github.com>
2024-03-31 18:09:06 -0400

    Rearranging divs

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const app=document.getElementById("app")
 const nav=document.getElementById("navbar")
 const arrows=document.getElementById("arrows")
+const food=document.getElementById("foodContainer")
 
 
 let sandwich=document.createElement('img')
 sandwich.setAttribute("src","sandwich.png")
 sandwich.setAttribute("alt","Sandwich")
-sandwich.setAttribute("height","10%")
 sandwich.setAttribute("class","characterSelect")
-app.appendChild(sandwich)
+food.appendChild(sandwich)
 
 let dessert=document.createElement('img')
 dessert.setAttribute("src","shortcake.png")
 dessert.setAttribute("alt","Dessert")
-dessert.setAttribute("height","10%")
 dessert.setAttribute("class","characterSelect")
-app.appendChild(dessert)
+food.appendChild(dessert)
+
+const sandwichMenu=(food)=>
+{
+    let item=document.createElement('div')
+    item.className="menu 1"
+}
